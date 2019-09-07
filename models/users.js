88b50b728a09f8ad029ad6fac57df48e93eb94fc
Programista3 <programista333@gmail.com>
2019-09-07 23:02:45 +0200

    Added top up ways list

--- models/users.js
+++ models/users.js
@@ -3,10 +3,10 @@ const Schema = mongoose.Schema;
 require('mongoose-type-email');
 
 const UsersSchema = new Schema({
-//	id: mongoose.Schema.Types.ObjectId,
 	firstname: String,
 	lastname: String,
 	email: mongoose.SchemaTypes.Email,
-	password: String
+	password: String,
+	balance: {type: Number, default: 500.0}
 });
-module.exports = mongoose.model('Users', UsersSchema, 'test1');
+module.exports = mongoose.model('Users', UsersSchema, 'users');
